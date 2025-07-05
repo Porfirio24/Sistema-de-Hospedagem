8e70660ced066f645338675fbaa32145db36bff1
Romulolevy <romulolevy437@gmail.com>
2025-07-05 15:00:38 -0300

    add quarto

---
+++ Src/Quarto.java
@@ -0,0 +1,42 @@
+public class Quarto {
+    private int numero;
+    private String tipo;
+    private double precoDiaria;
+    private boolean disponivel;
+
+    public Quarto(int numero, String tipo, double precoDiaria){
+        this.numero = numero;
+        this.tipo = tipo;
+        this.precoDiaria = precoDiaria;
+        this.disponivel = true; // o quanrto recebe o valor "true" pois sempre estara disponivel
+    }
+
+    public int getNumero(){
+        return numero;
+    }
+
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+
+    public  String getTipo(){
+        return tipo;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+
+    public double getPrecoDiaria(){
+        return precoDiaria;
+    }
+
+    public void serPrecoDiaria(){
+        this.precoDiaria = precoDiaria;
+    }
+
+    public boolean Disponivel(){
+        return disponivel;
+    }
+
+}
