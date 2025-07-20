081adafc85d60675ae61fc2522e0040aa701474e
Porfirio-Neto <netinholeite2607@gmail.com>
2025-07-20 19:56:39 -0300

    Refactor: reorganização Hospede

--- Src/models/Hospede.java
+++ Src/models/Hospede.java
@@ -1,48 +1,93 @@
-package Models;
+package models;
 
-public class Hospede {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Hospede implements Serializable {
+    private static final long serialVersionUID = 1L;
     private int id;
-    private String nome;
     private String cpf;
-    private String telefone;
+    private String nome;
     private String email;
+    private String senha;
+    private String telefone;
 
-    public Hospede(int id, String nome, String cpf, String telefone, String email) {
+    public Hospede(int id, String cpf, String nome, String email, String senha, String telefone) {
         this.id = id;
-        this.nome = nome;
         this.cpf = cpf;
-        this.telefone = telefone;
-        this.email = email;
-    }
-
-    public int getId() { return id; }
-    public String getNome() { return nome; }
-    public String getCpf() { return cpf; }
-    public String getTelefone() { return telefone; }
-    public String getEmail() { return email; }
-
-    @Override
-    public String toString() {
-        return id + " | " + nome + " | CPF: " + cpf + " | Tel: " + telefone + " | Email: " + email;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setNome(String nome) {
         this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.telefone = telefone;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getCpf() {
+        return cpf;
     }
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hospede hospede = (Hospede) o;
+        return id == hospede.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Hospede{" +
+                "id=" + id +
+                ", cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
     }
 }
