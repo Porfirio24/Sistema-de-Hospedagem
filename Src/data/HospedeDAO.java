f528466532208d85f3a6851ad6bddb8c9b028ba4
Porfirio-Neto <147106025+Porfirio24@users.noreply.github.com>
2025-07-20 22:24:33 -0300

    Refactor: adicionando HospedeDAO

---
+++ Src/data/HospedeDAO.java
@@ -0,0 +1,44 @@
+package controller;
+
+import data.HospedeDAO;
+import models.Hospede;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+public class HospedeController {
+    private final HospedeDAO hospedeDAO;
+
+    public HospedeController() {
+        this.hospedeDAO = new HospedeDAO();
+    }
+
+    public List<Hospede> buscarTodosHospedes() throws IOException, ClassNotFoundException {
+        return hospedeDAO.buscarTodos();
+    }
+
+    public Optional<Hospede> buscarHospedePorId(int id) throws IOException, ClassNotFoundException {
+        return hospedeDAO.buscarPorId(id);
+    }
+
+    public Optional<Hospede> buscarHospedePorCpf(String cpf) throws IOException, ClassNotFoundException {
+        return hospedeDAO.buscarPorCpf(cpf);
+    }
+
+    public Optional<Hospede> buscarHospedePorEmail(String email) throws IOException, ClassNotFoundException {
+        return hospedeDAO.buscarPorEmail(email);
+    }
+
+    public void cadastrarHospede(Hospede hospede) throws IOException, ClassNotFoundException {
+        hospedeDAO.salvar(hospede);
+    }
+
+    public void atualizarHospede(Hospede hospede) throws IOException, ClassNotFoundException {
+        hospedeDAO.salvar(hospede);
+    }
+
+    public void removerHospede(int id) throws IOException, ClassNotFoundException {
+        hospedeDAO.remover(id);
+    }
+}
